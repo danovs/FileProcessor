095f50ea5a37ef2569e6db4b54b5c19a7b5b2763
danovs <dsludanov@gmail.com>
2024-09-11 15:14:22 +0300

    Добавьте файлы проекта.

---
+++ FileProcessor/dllmain.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+extern "C" __declspec(dllexport) void ReplaceDigitsWithWord(const char* inputFilePath, const char* outputFilePath, int replaceCount);
+
+void ReplaceDigitsWithWord(const char* inputFilePath, const char* outputFilePath, int replaceCount) {
+    
+    ifstream inputFile(inputFilePath);
+    if (!inputFile.is_open()) {
+        throw runtime_error("Не удалось открыть входной файл.");
+    }
+
+    ofstream outputFile(outputFilePath);
+    if (!outputFile.is_open()) {
+        throw runtime_error("Не удалось открыть выходной файл.");
+    }
+
+    string line;
+    int count = 0;
+
+    while (getline(inputFile, line)) {
+        for (char& ch : line) {
+            if (isdigit(ch) && count < replaceCount) {
+                ch = ' ';
+                count++;
+            }
+        }
+        outputFile << line << endl;
+    }
+
+    // Проверка на выполнение замен
+    if (count < replaceCount) {
+        cerr << "Количество замен меньше запрошенного." << endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+}
